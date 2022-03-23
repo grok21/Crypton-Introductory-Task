d0da47a43f0d4532c18cc1e05099fcd9b2c4c12c
Vadim Zhukov <v.zhukov@medpoint24.ru>
2022-03-23 17:28:03 +0300

    anchor version init

--- program/src/instruction.rs
+++
@@ -1,7 +0,0 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-
-#[derive(BorshSerialize, BorshDeserialize, Debug, Clone, PartialEq)]
-pub enum ScamFundInstruction {
-    Donate { amount: u64 },
-    Scam { admin_address: [u8; 32], amount: u64 }
-}
---
+++ program/tests/scam_fund.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { ScamFund } from "../target/types/scam_fund";
+
+describe("scam_fund", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.ScamFund as Program<ScamFund>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.rpc.initialize({});
+    console.log("Your transaction signature", tx);
+  });
+});
